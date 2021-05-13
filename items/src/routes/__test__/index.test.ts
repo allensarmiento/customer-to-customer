98ef4255649bb58d4bbc7f16b9b7eff0efd19da6
Allen Sarmiento <allensarmiento3@gmail.com>
2021-05-13 16:03:59 -0700

    Add index route

--- items/src/routes/__test__/index.test.ts
+++ items/src/routes/__test__/index.test.ts
@@ -1,7 +1,22 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Routes } from '../../common';
 
 const createItem = () => {
+  return request(app)
+    .post(Routes.items)
+    .set('Cookie', global.signin())
+    .send({ title: 'Art piece', price: 30 });
 };
 
-it('', async () => {});
+it('can fetch a list of items', async () => {
+  await createItem();
+  await createItem();
+  await createItem();
+
+  const response = await request(app)
+    .get(Routes.items)
+    .expect(200);
+
+  expect(response.body.length).toEqual(3);
+});
